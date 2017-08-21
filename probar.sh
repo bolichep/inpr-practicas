990e7c4a6a908e598027f2c077d1d2815f05fab3
Pedro Boliche <bolichep@gmail.com>
2017-08-21 20:54:57 -0300

    add archivos soporte

---
+++ probar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for i in {1..9}
+do 
+	echo "vvvvvvvvvvvvvvvvvvvvvvvv Tablero$i"
+        pygobstones-lang tablero$i.gbb
+       	pygobstones-lang $1 tablero$i.gbb 2>&1
+	echo "^^^^^^^^^^^^^^^^^^^^^^^^ Tablero$i"
+done 
