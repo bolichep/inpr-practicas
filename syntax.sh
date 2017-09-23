b9b3a9fbb251ee34a3552a52629890588a1dabc7
Pedro Boliche <bolichep@gmail.com>
2017-09-23 20:24:42 -0300

    add check newer gobstones.vim to copy

--- syntax.sh
+++ syntax.sh
@@ -1,6 +1,6 @@
 # add line to filetype.vim and copy gobstones.vim to ~/.vim/syntax/
 [ -d ~/.vim ] || mkdir -p ~/.vim 
 [ -d ~/.vim/syntax ] || mkdir -p ~/.vim/syntax
-[ -f ~/.vim/syntax/gobstones.vim ] || cp gobstones.vim ~/.vim/syntax/.
-[ -f ~/.vim/filetype.vim ] || cp filetype.vim ~/.vim/. && diff -Dgbsgbsgbs filetype.vim ~/.vim/filetype.vim | sed "/gbsgbsgbs/d" > ~/.vim/filetype.vim
+[ ~/.vim/syntax/gobstones.vim -nt gobstones.vim ] || cp -v gobstones.vim ~/.vim/syntax/. 
+[ -f ~/.vim/filetype.vim ] || cp -v filetype.vim ~/.vim/. && diff -Dgbsgbsgbs filetype.vim ~/.vim/filetype.vim | sed "/gbsgbsgbs/d" > ~/.vim/filetype.vim
 
